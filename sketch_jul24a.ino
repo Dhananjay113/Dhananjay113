e9306eb3dc9c966b9bfb00877b1f858cc711c05f
Dhananjay113 <105308580+Dhananjay113@users.noreply.github.com>
2023-07-24 15:57:29 +0530

    Add files via upload

---
+++ sketch_jul24a.ino
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // Turn the LED on
+  delay(1000);                // Wait for 1 second (1000 milliseconds)
+  digitalWrite(ledPin, LOW);  // Turn the LED off
+  delay(1000);                // Wait for 1 second (1000 milliseconds)
+}
